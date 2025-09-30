7ce980d64c7aa8f3506b5c2486ca8714569fee93
GwansuKim <121069568+GwansuKim@users.noreply.github.com>
2025-09-30 09:22:42 +0900

    2의제곱XOR
    2의제곱XOR

--- Source.cpp
+++ Source.cpp
@@ -4,7 +4,33 @@ using namespace std;
 
 int main()
 {
+	cout << "숫자의 갯수를 입력해 주세요 : ";
 
+	int CountOfNumber = 0;
+	cin >> CountOfNumber;
+
+	int Result;
+	for (int i = 0; i < CountOfNumber; i++)
+	{
+		int InputNumber = 0;
+		cin >> InputNumber;
+
+		int POT = 1;
+		while (POT < InputNumber)
+		{
+			POT *= 2;
+		}
+
+		if (i == 0)
+		{
+			Result = POT;
+		}
+		else
+		{
+			Result ^= POT;
+		}
+	}
+	cout << "Result :: " << Result << endl;
 
 	return 0;
 }
